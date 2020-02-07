661effb5e39337184feb6f050f89fb241858ecb6
wxxiao33 <infinityxwx123@gmail.com>
2020-02-07 18:24:31 -0500

    hello page

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :instructors
   resources :movies
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'application#hello'
 end
