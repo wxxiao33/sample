5a09396aa83e4a58985cafeb55c81261128fe705
wxxiao33 <infinityxwx123@gmail.com>
2020-02-09 03:30:45 -0500

    Add Layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
   resources :subjects
   resources :courses
   resources :instructors
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  root 'static_pages#home'
+  get '/instructors', to: 'instructors#index'
+  get '/courses', to: 'courses#index'
+  get '/subjects', to: 'instructors#index'
 end
