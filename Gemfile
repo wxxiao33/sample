713dcb01830870b30ca10f0605e71d766f25d324
wxxiao33 <infinityxwx123@gmail.com>
2020-02-28 04:33:03 -0500

    Implement advanced login

--- Gemfile
+++ Gemfile
@@ -27,6 +27,8 @@ gem 'jbuilder', '~> 2.7'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
+gem 'jquery-rails'
+
 
 
 gem 'pry-byebug'
--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,8 @@ Rails.application.routes.draw do
   get '/users', to: 'users#new'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
+
+  # work around for jquery and turbolink not working
+  get '/logout',  to: 'sessions#destroy'
   resources :users
 end
